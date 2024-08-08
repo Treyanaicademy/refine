116cdd6cce740b3b961f95faa4afe608298379a8
Treyanaicademy <dubthea@gmail.com>
2024-08-08 03:44:22 -0500

    Update index.tsx
    All these imports are only used as types.biomelint/style/useImportType

--- examples/app-crm-minimal/src/routes/dashboard/components/total-count-card/index.tsx
+++ examples/app-crm-minimal/src/routes/dashboard/components/total-count-card/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import type React from "react";
 
 import { AuditOutlined, ShopOutlined, TeamOutlined } from "@ant-design/icons";
 import { Area, type AreaConfig } from "@ant-design/plots";
